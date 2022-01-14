5ca63cdef97b68886cfed2db60d8964954270405
Micael Brites da Silva <a21260203@isec.pt>
2022-01-14 13:29:45 +0000

    reserva create & confirm working

---
+++ MyAirbnb/Data/Migrations/20220113163024_displayDataFormated.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyAirbnb.Data.Migrations
+{
+    public partial class displayDataFormated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
